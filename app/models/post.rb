417d6c82d6c76e01fb713badb2727be2e8af82af
tomer <tomertal52@yahoo.com>
2023-02-01 11:03:56 -0500

    commit tree 22ff36d79fbc626c1118854ae61b2a356ec0047d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    validate :is_clickbaity
+
+    def is_clickbaity
+        if !(title.include? "Won't Believe")
+            errors.add(:title, "Clickbait-y material")
+        end
+    end
+
+
 end
